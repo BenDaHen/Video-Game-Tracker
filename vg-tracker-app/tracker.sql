c2da1bc9a25c797242e878778a545929294e0f78
Ben Wilmer <77129157+BenDaHen@users.noreply.github.com>
2026-05-22 15:16:37 -0600

    Started implementing SQLite database.

---
+++ vg-tracker-app/tracker.sql
@@ -0,0 +1,14 @@
+-- Table Structure
+
+-- CREATE TABLE games
+-- (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     cover TEXT,
+--     release_date TEXT
+-- );
+
+-- INSERT INTO games(id, name, cover, release_date) 
+-- VALUES(123456, 'Trails in the Sky 1st Chapter', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co96kj.webp', 'September 19, 2025');
+
+SELECT * FROM games;
